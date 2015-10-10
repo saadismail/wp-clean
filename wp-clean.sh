88821786f04ae2195a139147f3d76f171e6070f4
Saad <saad3112@gmail.com>
2015-10-10 10:23:47 +0000

    Replaced email with web address

--- wp-clean.sh
+++ wp-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Bash script written by Saad Ismail - saadismail.net
+# Bash script written by Saad Ismail - me@saadismail.net
 
 #Copyright (C) 2015 Saad Ismail
 #This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
